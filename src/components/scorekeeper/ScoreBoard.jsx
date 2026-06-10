b76d7c2e239e21d74a86871c47ac582f2c000514
Adrian Crabtree <illudcrab@gmail.com>
2026-06-10 18:36:06 +0000

    File changes

--- src/components/scorekeeper/ScoreBoard.jsx
+++ src/components/scorekeeper/ScoreBoard.jsx
@@ -199,8 +199,8 @@ export default function ScoreBoard({ players, winMode, bestOf, targetScore, last
         
         <div
           ref={scrollContainerRef}
-          className="flex flex-col h-full gap-2 w-full overflow-y-auto"
-          style={{ WebkitOverflowScrolling: "touch", paddingBottom: "96px" }}>
+          className="flex flex-col h-full gap-2 w-full overflow-y-auto relative z-0"
+          style={{ WebkitOverflowScrolling: "touch", paddingBottom: "140px" }}>
           
           <LayoutGroup>
             {sortedPlayers.map((player, idx) =>
